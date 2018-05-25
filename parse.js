322a7f1b0c46ac431b8fa8a771f4269695199dde
Jenny Zhang <canaantt@gmail.com>
2018-05-25 14:19:37 -0700

    implemented async loop in node to conver csv to json

--- parse.js
+++ parse.js
@@ -1,2 +1,31 @@
 // https://www.npmjs.com/package/csvtojson
+// node --max-old-space-size=3000 trying to mimic lambda specification
+
+const csv=require('csvtojson')
+var async = require("async");
+const testFolder = '.';
+const fs = require('fs');
+var csvFiles;
+fs.readdir(testFolder, (err, files) => {
+    csvFiles = files.filter(f => f.indexOf('.csv') > 0)
+})
+
+var csv2json = function(csvFilePath){
+    return new Promise((resolve, reject) => { 
+        csv()
+        .fromFile(csvFilePath)
+        .then((jsonObj)=>{
+            console.log(jsonObj.length);
+            resolve(jsonObj);
+        });
+    });
+}
+// Async / await usage
+var data;
+async.each(csvFiles, (file) =>{
+    console.log(file);
+    var result = csv2json(file);
+}, err => {
+    if (err) console.error(err.message);
+});
 
