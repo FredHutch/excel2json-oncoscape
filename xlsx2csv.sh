94e010b75a78dbbbf86ff7f6cfc12025c5c96eb6
Jenny Zhang <canaantt@gmail.com>
2018-05-25 13:06:51 -0700

    traverse through all sheets and export csv files using shell

--- xlsx2csv.sh
+++ xlsx2csv.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # xlsx2csv GBM-LGG.xlsx -s 1 > sheet1.csv
-
-max= 20
-for i in `seq 1 $max`
+file='GBM-LGG.xlsx'
+# max= 20
+for i in `seq 1 30`
 do
     echo "$i"
     SECONDS=0
-    xlsx2csv GBM-LGG.xlsx -s $i > sheet$i.csv
+    xlsx2csv $file -s $i > sheet$i.csv
+    lineNum=$(cat sheet$i.csv | wc -l)
+    echo "lineNum: $lineNum"
+    if [ "$lineNum" -eq 0 ]
+    then
+      rm sheet$i.csv
+      echo "sheet$i.csv is removed. Forloop stopped."
+      break
+    fi
     duration=$SECONDS
     echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 done
