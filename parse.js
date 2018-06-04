fbeb53def59015e6852210f33cff114e5a1e9cf7
Jenny Zhang <canaantt@gmail.com>
2018-06-04 13:00:54 -0700

    .

--- parse.js
+++ parse.js
@@ -19,6 +19,9 @@ var events = [];
 var jsonObj;
 var uploadResults = [];
 
+var gzip = zlib.createGzip({
+    level: 9 // maximum compression
+}), buffers=[], nread=0;
 
 console.log(csvFiles);
 var csv2json = function(csvFilePath) {
@@ -45,13 +48,16 @@ var serialization = function(jsonObj, file) {
     var regex_d = new RegExp('\-D');
     if (type === 'MATRIX') {
         var ids = Object.keys(jsonObj[0]);
+        ids = ids.map(id=>id.replace('tcga-', ''));         
+        ids = ids.map(id=>id.replace('TCGA-', ''));
         ids.splice(0, 1);
         var genes = jsonObj.map(j => j['Gene Symbol']);
         var values = jsonObj.map(dd => {
-            var val = _.values(dd).map(d=>parseFloat(d).toFixed(2));
+            var val = _.values(dd).map(d=>parseFloat(d));
             val.splice(0, 1);
             return val;
         });
+        obj.name = file.toLocaleLowerCase().replace('matrix-','').replace('-',' ');
         res.ids = ids;
         res.genes = genes;
         res.values = values;
@@ -60,6 +66,8 @@ var serialization = function(jsonObj, file) {
         var keys_uppercase = Object.keys(jsonObj[0]).map(k=>k.toUpperCase());
         var keys = Object.keys(jsonObj[0]);
         var ids = jsonObj.map(j=>j[keys[keys_uppercase.indexOf('PATIENTID')]]);
+        ids = ids.map(id=>id.replace('tcga-', ''));       
+        ids = ids.map(id=>id.replace('TCGA-', ''));
         var survival_keys = ['Vital Status', 'Days To Death', 'Days To Last Follow Up'];
         var fields = {};
         keys.splice(keys_uppercase.indexOf('PATIENTID'), 1);
@@ -74,6 +82,8 @@ var serialization = function(jsonObj, file) {
                 v = _.uniq(jsonObj.map(j=>j[k]));
                 if(v.indexOf(undefined) > -1) {
                     v.splice(v.indexOf(undefined), 1);
+                } else if (v.indexOf('NA') > -1) {
+                    v.splice(v.indexOf('NA'), 1);
                 }
             }
             fields[k] = v;
@@ -97,38 +107,54 @@ var serialization = function(jsonObj, file) {
         var keys_uppercase = Object.keys(jsonObj[0]).map(k=>k.toUpperCase());
         var keys = Object.keys(jsonObj[0]);
         var ids = jsonObj.map(j=>j[keys[keys_uppercase.indexOf('SAMPLEID')]]);
-        var fields = {};
-        keys.splice(keys_uppercase.indexOf('SAMPLEID'), 1);
-        keys.splice(keys_uppercase.indexOf('PATIENTID'), 1);
-        keys.forEach(k => {
-            if (k.match(regex_n) !== null) {
-                var col_values = jsonObj.map(j=>parseFloat(j[k]));
-                v = {
-                    'min' : _.min(col_values),
-                    'max' : _.max(col_values)
-                }
-            } else { //default type is string
-                v = _.uniq(jsonObj.map(j=>j[k]));
-                if(v.indexOf(undefined) > -1) {
-                    v.splice(v.indexOf(undefined), 1);
-                }
-            }
-            fields[k] = v;
+        ids = ids.map(id=>id.replace('tcga-', ''));         
+        ids = ids.map(id=>id.replace('TCGA-', ''));
+        var sampleIDLocation = keys_uppercase.indexOf('SAMPLEID');
+        var patientIDLocation = keys_uppercase.indexOf('PATIENTID');
+        var patient_keys = _.uniq(jsonObj.map(d=>d[keys[patientIDLocation]]));
+        var patientSampleMapping = {};
+        patient_keys.forEach(k=>{
+            var kk = k.replace('TCGA-', '');
+            patientSampleMapping[kk] = jsonObj.filter(d=>d[keys[patientIDLocation]]===k)
+                                             .map(d=>d[keys[sampleIDLocation]])
+                                             .map(d=>d.replace('TCGA-', ''));
         });
-        var values = jsonObj.map(j => {
-            var arr = [];
-            keys.forEach(k => {
-                if (k.match(regex_n) !== null) {
-                    arr.push(parseFloat(j[k]));
-                } else {
-                    arr.push(fields[k].indexOf(j[k]));
-                }
-            });
-            return arr;
-        });
-        res.ids = ids;
-        res.fields = fields;
-        res.values = values;
+
+        // var fields = {};
+        // keys.splice(keys_uppercase.indexOf('SAMPLEID'), 1);
+        // keys.splice(keys_uppercase.indexOf('PATIENTID'), 1);
+        // keys.forEach(k => {
+        //     if (k.match(regex_n) !== null) {
+        //         var col_values = jsonObj.map(j=>parseFloat(j[k]));
+        //         v = {
+        //             'min' : _.min(col_values),
+        //             'max' : _.max(col_values)
+        //         }
+        //     } else { //default type is string
+        //         v = _.uniq(jsonObj.map(j=>j[k]));
+        //         if(v.indexOf(undefined) > -1) {
+        //             v.splice(v.indexOf(undefined), 1);
+        //         }
+        //     }
+        //     fields[k] = v;
+        // });
+        // var values = jsonObj.map(j => {
+        //     var arr = [];
+        //     keys.forEach(k => {
+        //         if (k.match(regex_n) !== null) {
+        //             arr.push(parseFloat(j[k]));
+        //         } else {
+        //             arr.push(fields[k].indexOf(j[k]));
+        //         }
+        //     });
+        //     return arr;
+        // });
+        res = patientSampleMapping;
+        // res.ids = ids;
+        // res.fields = fields;
+        // res.values = values;
+        obj.name = 'psmap';
+        obj.type = 'psmap';
         obj.res = res;
     } else if (type === 'EVENT') {
         var map = {};
@@ -150,7 +176,7 @@ var serialization = function(jsonObj, file) {
         
         var values = jsonObj.map(d=>{
             var arr = [];
-            arr[0] = d[keys[patientIDLocation]];
+            arr[0] = d[keys[patientIDLocation]].replace('tcga-', '');
             arr[1] = parseInt(d[keys[startDateLocation]]);
             arr[2] = parseInt(d[keys[endDateLocation]]);
             var o = {};
@@ -171,6 +197,8 @@ var serialization = function(jsonObj, file) {
         var keys_uppercase = Object.keys(jsonObj[0]).map(k=>k.toUpperCase());
         var keys = Object.keys(jsonObj[0]);
         var ids = _.uniq(jsonObj.map(j => j[keys[keys_uppercase.indexOf('SAMPLEID')]]));
+        ids = ids.map(id=>id.replace('tcga-', ''));         
+        ids = ids.map(id=>id.replace('TCGA-', ''));
         var genes = _.uniq(jsonObj.map(j => j[keys[keys_uppercase.indexOf('GENE')]]));
         var mutTypes = _.uniq(jsonObj.map(j => j[keys[keys_uppercase.indexOf('TYPE')]]));
         var values = jsonObj.map((d)=>{
@@ -178,6 +206,8 @@ var serialization = function(jsonObj, file) {
                    genes.indexOf(d[keys[keys_uppercase.indexOf('GENE')]]) + '-' +
                    mutTypes.indexOf(d[keys[keys_uppercase.indexOf('TYPE')]]));
         });
+        res.name = 'mutations';
+        res.type = 'mut';
         res.ids = ids;
         res.genes = genes;
         res.mutationTypes = mutTypes;
@@ -197,7 +227,6 @@ fs.readdir(testFolder, (err, files) => {
                 var meta = {};
                 console.log('Done');
                 file = file.replace('.csv', '');
-                // heavy compute
                 var result = serialization(jsonObj, file);
                 var type = file.split('-')[0].toUpperCase();
                 if (type === 'EVENT') {
@@ -207,16 +236,20 @@ fs.readdir(testFolder, (err, files) => {
                     console.log('*****', file);
                     var jsonFileName = file + '.json.gz';
                     console.log('jsonFileName: ', jsonFileName);
-                    meta['name'] = file;
-                    meta['dataType'] = type;
-                    meta['file'] = jsonFileName;
+                    meta['name'] = res.name;
+                    meta['dataType'] = type.toLocaleLowerCase();
+                    meta['file'] = jsonFileName.replace('.gz','');
                     uploadResults.push(meta);
-                    var buf = new Buffer(JSON.stringify(result), 'utf-8');
-                    zlib.gzip(buf, level=9, function(err, result){
-                        jsonfile.writeFile(jsonFileName, result, function(err) {
-                            console.error(err);
-                        });
-                    });
+                    // jsonfile.writeFile(file + '.json', result.res, function(err){
+                    //     console.error(err);
+                    //     console.log(file + '.json is saved.');
+                    // });
+                    // var buf = new Buffer(JSON.stringify(result), 'utf-8');
+                    // zlib.gzip(buf, level=9, function(err, result){
+                    //     jsonfile.writeFile(jsonFileName, result, function(err) {
+                    //         console.error(err);
+                    //     });
+                    // });
                 }
                 callback();
             });
@@ -242,24 +275,41 @@ fs.readdir(testFolder, (err, files) => {
                 o.values = v;
                 obj.res = o;
                 data.push(obj);
+                jsonfile.writeFile('events.json', obj, function(err){
+                    console.error(err);
+                    console.log('events.json is saved.');
+                });
                 var jsonFileName = 'events.json.gz';
-                meta['name'] = 'EVENT';
-                meta['dataType'] = 'EVENT';
+                meta['name'] = 'events';
+                meta['dataType'] = 'events';
                 meta['file'] = jsonFileName;
                 uploadResults.push(meta);
+
                 var manifest = serialize.manifest(data, uploadResults);
                 jsonfile.writeFile('manifest.json', manifest, function(err){
-                        console.error(err);
-                        console.log('manifest is saved.');
-                    });
-                var buf = new Buffer(JSON.stringify(obj), 'utf-8');
-                zlib.gzip(buf, level=9, function(err, result){
-                    jsonfile.writeFile(jsonFileName, result, function(err) {
-                        console.error(err);
-                    });
+                    console.error(err);
+                    console.log('manifest is saved.');
                 });
+
+                // var buf = new Buffer(JSON.stringify(obj), 'utf-8');
+                // zlib.gzip(buf, level=9, function(err, result){
+                //     jsonfile.writeFile(jsonFileName, result, function(err) {
+                //         console.error(err);
+                //     });
+                // });
             } 
         }
       );
 });
+
+
+// var jsonFiles; 
+// fs.readdir('.', (err, files) => {
+//     jsonFiles = files.filter(f => f.indexOf('.json') > 0);
+// });
+// jsonFiles.forEach(jsonFile => {
+//     var r = fs.createReadStream('./' + jsonFile);
+//     var w = fs.createWriteStream('./' + jsonFile + '.gz');
+//     r.pipe(gzip).pipe(w);
+// });
 
